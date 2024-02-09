5999e2a22181833ad5788c6b8de3e8086f78bde9
Kevin Schneider <schneider.kev@outlook.de>
2024-02-09 17:29:49 +0100

    add full metadata to test script and index

---
+++ src/PackageRegistryService/StagingArea/test/test@2.0.0.fsx
@@ -0,0 +1,26 @@
+(*
+---
+Name: test
+MajorVersion: 1
+MinorVersion: 0
+PatchVersion: 0
+Description: this package is here for testing purposes only.
+Authors:
+  - FullName: John Doe
+    Email: j@d.com
+    Affiliation: University of Nowhere
+    AffiliationLink: https://nowhere.edu
+  - FullName: Jane Doe
+    Email: jj@d.com
+    Affiliation: University of Somewhere
+    AffiliationLink: https://somewhere.edu
+Tags:
+  - validation
+  - my-package
+  - thing
+ReleaseNotes: "add authors and tags for further testing"
+---
+*)
+
+// this file is intended for testing purposes only.
+printfn "Hello, world!"
--- update-index.fsx
+++ update-index.fsx
@@ -1,6 +1,6 @@
 #r "nuget: YamlDotNet, 13.7.1"
 #r "nuget: Fake.Core.Process, 6.0.0"
-#r "nuget: ARCValidationPackages, 2.0.0-preview.1"
+//#r "nuget: ARCValidationPackages, 2.0.0-preview.1" <-- use this (or ARCtrl) once the data model is stable
 
 open System
 open System.IO
@@ -8,7 +8,102 @@ open System.Text.Json
 open System.Text.Json.Serialization
 open YamlDotNet
 open YamlDotNet.Serialization
-open ARCValidationPackages
+// open ARCValidationPackages <-- use this (or ARCtrl) once the data model is stable
+
+// This is the F# version of /src/PackageRegistryService/Models/ValidationPackageIndex.cs
+// and should be equal to the implementation in arc-validate (until there is a common codebase/domain)
+[<AutoOpen>]
+module Domain = 
+
+    type Author() =
+        member val FullName = "" with get,set
+        member val Email = "" with get,set
+        member val Affiliation = "" with get,set
+        member val AffiliationLink = "" with get,set
+
+    type ValidationPackageMetadata() =
+        // mandatory fields
+        member val Name = "" with get,set
+        member val Description = "" with get,set
+        member val MajorVersion = 0 with get,set
+        member val MinorVersion = 0 with get,set
+        member val PatchVersion = 0 with get,set
+        // optional fields
+        member val Publish = false with get,set
+        member val Authors: Author [] = Array.empty<Author> with get,set
+        member val Tags: string [] = Array.empty<string> with get,set
+        member val ReleaseNotes = "" with get,set
+
+        override this.GetHashCode() =
+            hash (
+                this.Name, 
+                this.Description, 
+                this.MajorVersion, 
+                this.MinorVersion, 
+                this.PatchVersion, 
+                this.Publish,
+                this.Authors,
+                this.Tags,
+                this.ReleaseNotes
+            )
+
+        override this.Equals(other) =
+            match other with
+            | :? ValidationPackageMetadata as vpm -> 
+                (
+                    this.Name, 
+                    this.Description, 
+                    this.MajorVersion, 
+                    this.MinorVersion, 
+                    this.PatchVersion, 
+                    this.Publish,
+                    this.Authors,
+                    this.Tags,
+                    this.ReleaseNotes
+                ) = (
+                    vpm.Name, 
+                    vpm.Description, 
+                    vpm.MajorVersion, 
+                    vpm.MinorVersion, 
+                    vpm.PatchVersion, 
+                    vpm.Publish,
+                    vpm.Authors,
+                    vpm.Tags,
+                    vpm.ReleaseNotes
+                )
+            | _ -> false
+
+    type ValidationPackageIndex =
+        {
+            RepoPath: string
+            FileName:string
+            LastUpdated: System.DateTimeOffset
+            Metadata: ValidationPackageMetadata
+        } with
+            static member create (
+                repoPath: string, 
+                fileName: string, 
+                lastUpdated: System.DateTimeOffset,
+                metadata: ValidationPackageMetadata
+
+            ) = 
+                { 
+                    RepoPath = repoPath 
+                    FileName = fileName
+                    LastUpdated = lastUpdated 
+                    Metadata = metadata
+                }
+            static member create (
+                repoPath: string, 
+                lastUpdated: System.DateTimeOffset,
+                metadata: ValidationPackageMetadata
+            ) = 
+                ValidationPackageIndex.create(
+                    repoPath = repoPath,
+                    fileName = Path.GetFileNameWithoutExtension(repoPath),
+                    lastUpdated = lastUpdated,
+                    metadata = metadata
+                )
 
 [<AutoOpen>]
 module Frontmatter = 
