61b5226da2315683a35ff91fe9706467961c5173
Kevin Schneider <schneider.kev@outlook.de>
2024-02-16 16:17:22 +0100

    Add POC F# client project

---
+++ src/AVPRClient/Client.fs
@@ -0,0 +1,8 @@
+﻿namespace AVPRClient
+
+module AVPR_V1 =
+
+    let getAllPackages() =
+        Globals.Client_V1.GetAllPackages()
+        |> Async.AwaitTask
+        |> Async.RunSynchronously
---
+++ src/AVPRClient/Globals.fs
@@ -0,0 +1,11 @@
+﻿namespace AVPRClient
+
+open SwaggerProvider
+
+module Globals =
+
+    let [<Literal>] AVPR_SCHEMA_V1 = "https://avpr.nfdi4plants.org/swagger/v1/swagger.json"
+
+    type AVPR = OpenApiClientProvider<AVPR_SCHEMA_V1>
+
+    let Client_V1 = AVPR.Client()
