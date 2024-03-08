6589e9fad319a8e452ebb6fa44c299d3af7ccede
Kevin Schneider <schneider.kev@outlook.de>
2024-03-08 11:36:32 +0100

    pipeline test

---
+++ src/PackageRegistryService/StagingArea/test/test@0.0.1.fsx
@@ -0,0 +1,29 @@
+(*
+---
+Name: test
+Summary: this package is here for testing purposes only.
+Description: this package is here for testing purposes only.
+MajorVersion: 0
+MinorVersion: 0
+PatchVersion: 1
+Publish: true
+---
+*)
+ 
+// this file is intended for testing purposes only.
+printfn "If you can read this in your console, you successfully executed test package v1.0.0!" 
+
+#r "nuget: ARCExpect, 1.0.1"
+
+open ARCExpect
+open Expecto
+
+let validationCases = testList "test" [
+    test "yes" {Expect.equal 1 1 "yes"}
+]
+
+validationCases
+|> Execute.ValidationPipeline(
+    basePath = System.Environment.CurrentDirectory,
+    packageName = "test"
+)
