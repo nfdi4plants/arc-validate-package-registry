911beaf46f269573eb8090b90eecebe23326820b
Kevin Schneider <schneider.kev@outlook.de>
2024-06-11 16:18:37 +0200

    add a test package that uses ARCExpect v3

---
+++ StagingArea/test/test@5.0.0.fsx
@@ -0,0 +1,42 @@
+let [<Literal>]PACKAGE_METADATA = """(*
+---
+Name: test
+MajorVersion: 4
+MinorVersion: 0
+PatchVersion: 0
+Publish: true
+Summary: this package is here for testing purposes only.
+Description: this package is here for testing purposes only.
+Authors:
+  - FullName: John Doe
+    Email: j@d.com
+    Affiliation: University of Nowhere
+    AffiliationLink: https://nowhere.edu
+  - FullName: Jane Doe
+    Email: jj@d.com
+    Affiliation: University of Somewhere
+    AffiliationLink: https://somewhere.edu
+Tags:
+  - Name: validation
+  - Name: my-package
+  - Name: thing
+ReleaseNotes: "use in-package metadata"
+---
+*)"""
+
+#r "nuget: ARCExpect, 3.0.0"
+
+open ARCExpect
+open Expecto
+let test_package =
+    Setup.ValidationPackage(
+        metadata = Setup.Metadata(PACKAGE_METADATA),
+        CriticalValidationCases = [
+            test "yes" {Expect.equal 1 1 "yes"}
+        ]
+    )
+
+test_package
+|> Execute.ValidationPipeline(
+    basePath = System.Environment.CurrentDirectory
+)
