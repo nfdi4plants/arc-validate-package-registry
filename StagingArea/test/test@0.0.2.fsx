a1a33e7ed9c45062bfc29bbf04934fbea17bc8b2
Kevin Schneider <schneider.kev@outlook.de>
2024-06-19 15:08:23 +0200

    use new tool in CI, add a test package to test pipeline

---
+++ StagingArea/test/test@0.0.2.fsx
@@ -0,0 +1,14 @@
+(*
+---
+Name: test
+Summary: this package is here for testing purposes only.
+Description: this package is here for testing purposes only.
+MajorVersion: 0
+MinorVersion: 0
+PatchVersion: 2
+Publish: false
+---
+*)
+ 
+// this file is intended for testing purposes only.
+printfn "If you can read this in your console, you successfully executed test package v0.0.2!" 
