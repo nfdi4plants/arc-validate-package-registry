c1a660d73682c2a7714ad0fb69db82833a46428f
Dominik Brilhaus <brilhaus@nfdi4plants.org>
2025-05-22 14:34:03 +0200

    rename and adapt cmml package

---
+++ StagingArea/hhu-cmml/cmml-plantsamples@0.0.1.fsx
@@ -0,0 +1,129 @@
+let [<Literal>]PACKAGE_METADATA = """(*
+---
+Name: hhu-cmml
+Summary: Validates if the ARC contains the necessary metadata for plant sample submission to HHU CMML.
+Description: |
+  Validates if the ARC contains the necessary metadata for plant sample submission to HHU CMML.
+  This is validates against "Swate template study sheet for plant samples" 
+  https://str.nfdi4plants.org/template/226689ec-4be3-4143-b775-f8856c8ed6a5
+  The following metadata is required (may not be empty):
+  - Parameter [sample submission date]
+  - Characteristic [organism]
+  - Characteristic [biological replicate]
+  The following metadata is recommended:
+  - Characteristic [organism part]
+MajorVersion: 0
+MinorVersion: 0
+PatchVersion: 1
+Publish: false
+Authors:
+  - FullName: Dominik Brilhaus
+    Affiliation: CEPLAS
+    AffiliationLink: https://ceplas.eu
+Tags:
+  - Name: ARC
+  - Name: CMML
+ReleaseNotes: |
+  - First version of CMML plant sample sheet validation package
+---
+*)"""
+
+#r "nuget: ARCExpect, 4.0.0"
+
+open ControlledVocabulary
+open Expecto
+open ARCExpect
+open ARCTokenization
+open ARCTokenization.StructuralOntology
+open System.IO
+open System.Text
+open FSharpAux
+
+// Input:
+// let arcDir = Directory.GetCurrentDirectory()
+
+// TEST
+let arcDir = @"/Users/dominikbrilhaus/Downloads/cmml-sample-validate"
+
+// Values:
+let absoluteDirectoryPaths = FileSystem.parseARCFileSystem arcDir
+
+let studyFiles = 
+    try 
+        absoluteDirectoryPaths
+        |> Study.parseProcessGraphColumnsFromTokens arcDir
+    with
+        | _ -> seq{Map.empty}
+
+let containsFilledOutColumn (term : CvTerm) (tokenColumns : IParam list list) =
+    let column = 
+        tokenColumns 
+        |> Seq.tryFind (fun column ->
+            Param.getValueAsTerm column.Head = term
+        )
+    match column with
+    | Some (h :: []) -> Expecto.Tests.failtestNoStackf $"{term.Name} column only contains header"            
+    | Some (h :: vals) -> 
+        vals
+        |> List.iteri (fun i token ->
+            if (Param.getValueAsTerm token).Name = "" then
+                Expecto.Tests.failtestNoStackf $"column {term.Name} contains empty value at index {i}"                  
+        )
+    | _ -> Expecto.Tests.failtestNoStackf $"table contains no {term.Name} header" 
+
+// 
+
+let plantGrowthSheets = 
+    studyFiles
+    |> Seq.collect (fun s ->
+        s
+        |> Seq.filter (fun kv ->
+            kv.Value 
+            |> Seq.concat
+            |> Seq.exists (fun token -> 
+                token.Name = "ProtocolType" 
+                &&
+                (Param.getValueAsTerm token).Name = "plant growth protocol"
+            )
+        )   
+    )
+           
+
+// Validation Cases:
+let cases = 
+    testList "cases" [  // naming is difficult here
+
+        ARCExpect.validationCase (TestID.Name "plant growth table") {
+            if plantGrowthSheets |> Seq.isEmpty then
+                Expecto.Tests.failtestNoStackf "No plant growth table found"            
+        }
+
+        if plantGrowthSheets |> Seq.isEmpty |> not then
+            for table in plantGrowthSheets do
+                ARCExpect.validationCase (TestID.Name $"{table.Key}: organism") {
+                    table.Value
+                    |> containsFilledOutColumn (CvTerm.create("OBI:0100026","organism","OBI"))
+                }
+    ]
+
+let nonCriticalCases = 
+    testList "cases" [  // naming is difficult here
+
+        if plantGrowthSheets |> Seq.isEmpty |> not then
+            for table in plantGrowthSheets do
+               
+                ARCExpect.validationCase (TestID.Name $"{table.Key}: organism part") {
+                    table.Value
+                    |> containsFilledOutColumn (CvTerm.create("EFO:0000635","organism part","EFO"))
+                }
+    ]
+
+// Execution:
+Setup.ValidationPackage(
+    metadata = Setup.Metadata(PACKAGE_METADATA),
+    CriticalValidationCases = [cases],
+    NonCriticalValidationCases = [nonCriticalCases]
+)
+|> Execute.ValidationPipeline(
+    basePath = arcDir
+)
